276bc6e8dd5ad4e9fcbbdddb5d31ac7807b6e05f
Dipo Rodipe <Wither-Bane@users.noreply.github.com>
2023-07-01 18:31:29 +0100

    I have modified the example code to demonstrate the use of the earlier example website being referred to in my flask app's root webserver folder route

--- server.py
+++ server.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 print(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, D!</p>"
+    return render_template('index.html')
 
 @app.route('/blog')
 def blog():
