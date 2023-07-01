fa58fb65a2e6193d70603ee7e3cbb7f21567e1be
Dipo Rodipe <Wither-Bane@users.noreply.github.com>
2023-07-01 18:23:19 +0100

    added some new routes, demonstrating that I can add new pages with longer URLs than simply the root of the website

--- server.py
+++ server.py
@@ -5,4 +5,12 @@ print(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, Dipo!</p>"
+    return "<p>Hello, D!</p>"
+
+@app.route('/blog')
+def blog():
+    return 'These are my thoughts on blogs'
+
+@app.route('/blog/2020/dogs')
+def blog2():
+    return 'This is my dog'
