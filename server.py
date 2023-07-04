1f629aec967fc2619e43fd6b7d2d7344a85a25b2
Dipo Rodipe <Wither-Bane@users.noreply.github.com>
2023-07-04 21:12:21 +0100

    added ability to send request data and perform an action when a user clicks the submit form button. Only created the route in server.py without implementing the reference in contact.html as it was already built for me

--- server.py
+++ server.py
@@ -10,3 +10,16 @@ def my_home_default():
 @app.route("/<string:page_name>")
 def html_page(page_name):
     return render_template(page_name)
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    # error = None
+    # if request.method == 'POST':
+    #     if valid_login(request.form['username'],
+    #                    request.form['password']):
+    #         return log_the_user_in(request.form['username'])
+    #     else:
+    #         error = 'Invalid username/password'
+    # # the code below is executed if the request method
+    # # was GET or the credentials were invalid
+    return 'form submitted hooorayyy!'
