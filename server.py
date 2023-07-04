9fa34894abe32f9ff671585a6bbb45b657c9306a
Dipo Rodipe <Wither-Bane@users.noreply.github.com>
2023-07-04 11:48:10 +0100

    fixed CSS and javascript references in the index.html page, and added routes for the works, contact and home pages (as 'about me' already has an about.html route)

--- server.py
+++ server.py
@@ -4,17 +4,21 @@ app = Flask(__name__)
 print(__name__)
 
 @app.route("/")
+def my_home_default():
+    return render_template("index.html")
+
+@app.route("/index.html")
 def my_home():
-    return render_template('index.html')
+    return render_template("index.html")
 
 @app.route("/about.html")
 def about():
-    return render_template('about.html')
+    return render_template("about.html")
 
-@app.route('/blog')
-def blog():
-    return 'These are my thoughts on blogs'
+@app.route("/works.html")
+def works_page():
+    return render_template("works.html")
 
-@app.route('/blog/2020/dogs')
-def blog2():
-    return 'This is my dog'
+@app.route("/contact.html")
+def contact_page():
+    return render_template("contact.html")
