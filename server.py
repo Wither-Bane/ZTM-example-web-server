bc0ec538997d6bab65e6ddb6fd3b0618ccba5c8a
Dipo Rodipe <Wither-Bane@users.noreply.github.com>
2023-07-04 18:40:23 +0100

    made generic variable rule for all routes one level deep from the root route, removing the other specific references to the main pages

--- server.py
+++ server.py
@@ -7,23 +7,6 @@ print(__name__)
 def my_home_default():
     return render_template("index.html")
 
-@app.route("/index.html")
-def my_home():
-    return render_template("index.html")
-
-@app.route("/about.html")
-def about():
-    return render_template("about.html")
-
-@app.route("/works.html")
-def works_page():
-    return render_template("works.html")
-
-@app.route("/contact.html")
-def contact_page():
-    return render_template("contact.html")
-
-
-@app.route("/work.html")
-def work_alt_page():
-    return render_template("work.html")
+@app.route("/<string:page_name>")
+def html_page(page_name):
+    return render_template(page_name)
